a35cb89703b63abe6730b2961f2ba45eb0ba0030
SweetCapitan <34382881+sweetcapitan@users.noreply.github.com>
2020-01-30 01:44:19 +0300

    Пофикшена функция с реакциями бота(Наверное)

--- modules/Chat.py
+++ modules/Chat.py
@@ -3,7 +3,6 @@ import discord
 from discord.ext import commands
 # from main import result_embed, pluralize
 
-notification_channel = 531622332859547668
 
 async def result_embed(result_state, description, message):
     embed = discord.Embed(title=result_state, description=description, color=0xd5de21)
@@ -103,11 +102,12 @@ class Chat(commands.Cog):
             if emo.lower() in message.content.lower():
                 emoji = emo
                 await message.add_reaction(emoji)
+        self.bot.process_commands(message)
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.content.lower() == 'пошел нахуй' or message.content.lower() == 'нахуй пошел':
-            await message.channel.send('```' + text + '```')
+            await message.channel.send(f'```{text}```')
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, _):
@@ -115,17 +115,6 @@ class Chat(commands.Cog):
             if emo.lower() in str(reaction).lower():
                 emoji = emo
                 await reaction.message.add_reaction(emoji)
-
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        channel = self.bot.get_channel(notification_channel)
-        await channel.send(f'{member} присоеденился к нам. Земля ему говном.')
-
-    @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        channel = self.bot.get_channel(notification_channel)
-        await channel.send(f'{member} покинул нас. Земля ему говном.')
-
 
 
 def setup(bot):
