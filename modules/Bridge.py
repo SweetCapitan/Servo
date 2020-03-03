a6eb14e4ac57c69056e8e34c5beb50956d72415c
SweetCapitan <34382881+SweetCapitan@users.noreply.github.com>
2020-03-03 13:31:59 +0300

    Нахуй не нужный Bridge.py, сделанный от скуки. Скорее всего через пару дней канет в забвение

---
+++ modules/Bridge.py
@@ -0,0 +1,29 @@
+import sys
+import cyrtranslit
+import os
+import discord
+import requests
+from discord.ext import commands
+sys.path.append('..')
+from Lib import Logger, pluralize
+
+BRIDGE_URL = os.environ.get('BRIDGE_URL')
+
+
+class Bridge(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    logger = Logger()
+
+    @commands.command()
+    async def m(self, ctx):
+        member_name = ctx.message.guild.get_member(ctx.message.author.id).nick
+        data = {'message': (f'{member_name}: {cyrtranslit.to_latin(ctx.message.content[3:])}', 'ru')}
+        print(data)
+        # requests.put(BRIDGE_URL, data=data)
+
+
+def setup(bot):
+    bot.add_cog(Bridge(bot))
