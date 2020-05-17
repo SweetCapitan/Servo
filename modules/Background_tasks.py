87ec569c0d647460c9abefde3d179505b1d538d0
SweetCapitan <34382881+SweetCapitan@users.noreply.github.com>
2020-05-17 23:56:13 +0300

    Я еблан

--- modules/Background_tasks.py
+++ modules/Background_tasks.py
@@ -28,7 +28,7 @@ with conn:
 
 with conn:
     with conn.cursor() as cur:
-        cur.execute("INSERT INTO covidtime (time) VALUES (%s)", '1589684400')
+        cur.execute("INSERT INTO covidtime (time) VALUES (%s)" % '1589684400')
         cur.execute("SELECT time FROM covidtime")
         response_time = cur.fetchone()
 
