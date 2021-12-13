f6a37efa61f8b2b02fce66e3183fea44e7635388
Sweetcapitan <dangho11@mail.ru>
2021-12-13 06:11:05 +0300

    [Update] update webhook

--- Utilities/webhook.py
+++ Utilities/webhook.py
@@ -1,8 +1,8 @@
 import aiohttp
-from discord.webhook import Webhook
+from discord.webhook import Webhook, AsyncWebhookAdapter
 
 
 async def send_webhook(webhook_url: str, text: str):
     async with aiohttp.ClientSession() as session:
-        webhook = Webhook.from_url(webhook_url, session=session)
+        webhook = Webhook.from_url(webhook_url, adapter=AsyncWebhookAdapter(session))
         await webhook.send(text)
