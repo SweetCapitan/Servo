dca5a60b2a17ce10b98a92e81be35ebd86665b96
Sweetcapitan <danil.duhanin@yandex.ru>
2022-02-01 11:45:13 +0300

    fix session requirement

--- Utilities/webhook.py
+++ Utilities/webhook.py
@@ -1,16 +1,10 @@
 import aiohttp
-<<<<<<< HEAD
-from discord.webhook import Webhook, AsyncWebhookAdapter
-=======
 from discord.webhook import Webhook
->>>>>>> e1b86f4 ([Update] Lib.py is spliced into other small files)
+from discord.webhook import Webhook
 
 
 async def send_webhook(webhook_url: str, text: str):
     async with aiohttp.ClientSession() as session:
-<<<<<<< HEAD
-        webhook = Webhook.from_url(webhook_url, adapter=AsyncWebhookAdapter(session))
-=======
         webhook = Webhook.from_url(webhook_url, session=session)
->>>>>>> e1b86f4 ([Update] Lib.py is spliced into other small files)
+        webhook = Webhook.from_url(webhook_url, session=session)
         await webhook.send(text)
