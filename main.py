4c980b99b3a74e13890c462395de707bb82d218f
Sweetcapitan <danil.duhanin@yandex.ru>
2022-02-03 12:30:04 +0300

    Update main

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import time
 
-import discord
+import requests
+import discord.ext.commands
+from discord import Option
 from discord.ext import commands
-from discord.commands import Option, slash_command
+from discord.ext.commands import slash_command
 from discord.ext.commands.context import Context
 from discord.ui.view import View
 
 import sys
 import os
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
+import config
 from Utilities import logger
 from Utilities.embeds import pluralize, ResultEmbeds
 from Utilities.webhook import send_webhook
@@ -20,6 +20,7 @@ db = ServoMySQL()
 bot_start_time = time.time()
 re = ResultEmbeds()
 module_list_choices = []
+
 
 class Bot(commands.Bot):
     def __init__(self, command_prefix, **options):
@@ -40,14 +41,15 @@ class Bot(commands.Bot):
         logger.log(f'Всего Модулей: {count}')
         logger.log('Всего Команд: {}'.format(len(self.all_commands)))
         module_list_choices.append("all")
-        # try:
-        #     await self.sync_commands()
-        # except Exception as Ex:
-        #     # await send_webhook(os.environ.get('WH_URL'), 'Ошибка Сихронизации slash команд!')
-        #     logger.warn(f'Произошла ошибка при синхронизации slash команд!\n {Ex}')
-        #     await discord.Bot.close(self)
-        # else:
-        #     logger.log('Slash команды синхронизированны!')
+        try:
+            await self.register_commands()
+            # await self.sync_commands()
+        except Exception as Ex:
+            # await send_webhook(os.environ.get('WH_URL'), 'Ошибка Сихронизации slash команд!')
+            logger.warn(f'Произошла ошибка при синхронизации slash команд!\n {Ex}')
+            await discord.Bot.close(self)
+        else:
+            logger.log('Slash команды синхронизированны!')
         logger.log(db.init())
         logger.log('=====================================================')
         bot_time = round(time.time() - bot_start_time)
@@ -57,47 +59,48 @@ class Bot(commands.Bot):
 
 bot = Bot(command_prefix='?', owner_id=252469010308792322,
           description='Этот бот напсан чисто по фану, для использования на собственном сервере. Если возникли вопросы, '
-                      'то прошу ко мне на сервер https://discord.gg/4c7TbSd')
-
-# module_List_Choices = [str(file[:-3]) for file in os.listdir('modules') if file.endswith('.py')].append("all")
+                      'то прошу ко мне на сервер https://discord.gg/4c7TbSd', intents=discord.Intents.default())
 
 
-@slash_command(guild_ids=[os.environ.get("SERVER_ID")], name='reload',
-               description='Перезагрузка модуля из памяти бота')
-async def reload(ctx, module: Option(str,
-                                     choices=module_list_choices, description="модуль, который будет перезапущен"), ):
-    """ ХУЙ ПИЗДА СКОВРОДА"""
-    if not ctx.author.guild_permissions.manage_messages:  # Костыльно Ориентированное Программирование в деле
-        return ctx.respond("Прав не завезли!", ephemeral=True)
-    if str(module) == 'all':
-        count = 0
-        module_list = []
-        for file in os.listdir('modules'):
-            if file.endswith('.py'):
-                module_list.append(file[:-3] + '\n')
-                bot.unload_extension(f'modules.{file[:-3]}')
-                bot.load_extension(f'modules.{file[:-3]}')
-                logger.log(f'Reload Module: {file}')
-                count += 1
-        module_list_text = ''
-        for mod in module_list:
-            module_list_text = module_list_text + mod
-        logger.comm(f'RELOAD module: [{module}]. Author: {ctx.author}')
-        return await ctx.respond(embed=re.done(f'Всего модулей перезагружено: {count}\n{module_list_text}'))
-    else:
-        bot.unload_extension(f'modules.{module}')
-        bot.load_extension(f'modules.{module}')
-        logger.comm(f'RELOAD module: [{module}]. Author: {ctx.author}')
-        return await ctx.respond(embed=re.done(f'Модуль [{module}] перезагружен'))
+class Main(commands.Cog):
+    @slash_command(guild_ids=[int(os.environ.get("SERVER_ID"))], name='reload',
+                   description='Перезагрузка модуля из памяти бота')
+    async def reload(self, ctx, module: Option(str,
+                                               choices=module_list_choices,
+                                               description="модуль, который будет перезапущен"), ):
+        if not ctx.author.guild_permissions.manage_messages:  # Костыльно Ориентированное Программирование в деле
+            return ctx.respond("Прав не завезли!", ephemeral=True)
+        if str(module) == 'all':
+            count = 0
+            module_list = []
+            for file in os.listdir('modules'):
+                if file.endswith('.py'):
+                    module_list.append(file[:-3] + '\n')
+                    bot.unload_extension(f'modules.{file[:-3]}')
+                    bot.load_extension(f'modules.{file[:-3]}')
+                    logger.log(f'Reload Module: {file}')
+                    count += 1
+            module_list_text = ''
+            for mod in module_list:
+                module_list_text = module_list_text + mod
+            logger.comm(f'RELOAD module: [{module}]. Author: {ctx.author}')
+            await ctx.respond(embed=re.done(f'Всего модулей перезагружено: {count}\n{module_list_text}'))
+        else:
+            bot.unload_extension(f'modules.{module}')
+            bot.load_extension(f'modules.{module}')
+            logger.comm(f'RELOAD module: [{module}]. Author: {ctx.author}')
+            await ctx.respond(embed=re.done(f'Модуль [{module}] перезагружен'))
+
+    @slash_command(guild_ids=[int(os.environ.get("SERVER_ID"))], name='load',
+                   description='Загружает модуль в память бота, до его перезагрузки', )
+    async def load(self, ctx, extension: Option(str, choices=module_list_choices,
+                                                description="модуль, который будет перезапущен")):
+        if not ctx.author.guild_permissions.manage_messages:
+            return ctx.respond("Прав не завезли!", ephemeral=True)
+        bot.load_extension(f'modules.{extension}')
+        logger.comm(f'LOAD module: {extension}. Author: {ctx.message.author}')
+        await ctx.respond(embed=re.done(f'Модуль [{extension}] Загружен'))
 
 
-@slash_command(guild_ids=[os.environ.get("SERVER_ID")], name='load',
-               description='Загружает модуль в память бота, до его перезагрузки', )
-async def load(self, ctx, extension: Option(str, choices=module_list_choices,
-                                            description="модуль, который будет перезапущен")):
-    self.bot.load_extension(f'modules.{extension}')
-    await re.done(f'Модуль [{extension}] Загружен')
-    self.logger.comm(f'LOAD module: {extension}. Author: {ctx.message.author}')
-
-
+bot.add_cog(Main())
 bot.run(os.environ.get('BOT_TOKEN'))
